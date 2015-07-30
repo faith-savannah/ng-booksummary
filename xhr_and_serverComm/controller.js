b0a7130dcbbc695aa19a57bceeec67687e0e7fbb
faith kangai <faith.kangai@savannahinformatics.com>
2015-07-30 17:07:53 +0300

    XHR and server communication

---
+++ xhr_and_serverComm/controller.js
@@ -0,0 +1,27 @@
+angular.module('myApp', [])
+.controller('ServiceController', ['$scope', '$timeout', 'githubService',
+  function($scope, $timeout, githubService) {
+    var timeout;
+    $scope.$watch('username', function(newVal) {
+      if (newVal) {
+        if (timeout) $timeout.cancel(timeout);//restart the timer
+        timeout = $timeout(function() {
+          githubService.events(newVal)
+          .success(function(data, status) {
+            $scope.events = data.data;
+          });
+        }, 1000);
+      }
+    });
+  }])
+.factory('githubService', ['$http', function($http) {
+  var doRequest = function(username, path) {
+    return $http({
+      method: 'JSONP',
+      url: 'https://api.github.com/users/' + username + '/' + path + '?callback=JSON_CALLBACK'
+    });
+  }
+  return {
+    events: function(username) { return doRequest(username, 'events'); },
+  };
+}]);
